e739499c428a4e7db886c12eb7281aaa166ac75d
Gabriel Cerentini Farias <64269858+Arch-G-C@users.noreply.github.com>
2021-06-13 17:43:31 -0300

    SojaNovo

--- Soja.sh
+++ Soja.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-result1=$(wget -qO- http://fpuntel.000webhostapp.com/soja/soja1.php)
+tel.000webhostapp.com/soja/soja1.php)
 result2=$(wget -qO- http://fpuntel.000webhostapp.com/soja/soja2.php)
 result3=$(wget -qO- http://fpuntel.000webhostapp.com/soja/soja3.php)
-result4=$(wget -qO- http://fpuntel.000webhostapp.com/soja/estacaoMeteorologico.php)
+result4=$(wget -qO- http://fpuntel.000webhostapp.com/soja/estacaoMeteorologica.php)
 result5=$(wget -qO- http://fpuntel.000webhostapp.com/soja/previsaoTempo.php)
 
-echo -n result5|cut -c 1-3 >> Soja.csv
+echo -n result5|cut -c 1-3 > Soja.csv
